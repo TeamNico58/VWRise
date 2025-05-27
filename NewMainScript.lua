fc7bb75c461cae091875a32a4ba30d1a282eacca
TeamNico58 <108380973+TeamNico58@users.noreply.github.com>
2025-05-26 20:30:33 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -354,7 +354,7 @@ local function are_installed_2()
 end
 if not are_installed_1() then install_profiles(1) end
 if not are_installed_2() then install_profiles(2) end
-local url = RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VoidwareBakup"
+local url = RiseMode and "https://github.com/TeamNico58/VWRise/" or "https://github.com/VapeVoidware/VoidwareBakup"
 if not shared.VapeDeveloper then 
 	local commit = "main"
 	for i,v in pairs(game:HttpGet(url):split("\n")) do 
